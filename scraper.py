0ab2c471ad7637e29aee5e7d7cfb44f36889a692
AlphaZuloZulo <yunli2012@gmail.com>
2015-10-18 21:12:19 -0400

    2015 D

--- scraper.py
+++ scraper.py
@@ -17,7 +17,8 @@ lstring = ', '.join(lookup)
 
 scraperwiki.sqlite.execute('create table if not exists score (%s)'%lstring)
 
-url='http://sports.yahoo.com/nhl/stats/byposition?pos=D'
+url='http://sports.yahoo.com/nhl/stats/byposition?pos=D&conference=NHL&year=season_2015&qualified=1'
+#url='http://sports.yahoo.com/nhl/stats/byposition?pos=D'
 #url='http://sports.yahoo.com/nhl/stats/byposition?pos=C,RW,LW,D'
 html=requests.get(url).content
 root=lxml.html.fromstring(html)
